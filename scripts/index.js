1f03b61f0b62835d11bc344f8c27e0fa268474f4
Margarita Panfilova <ritckapanfilova@yandex.ru>
2022-06-07 16:18:16 +0800

    finish popup

--- scripts/index.js
+++ scripts/index.js
@@ -1,34 +1,30 @@
-const button = document.querySelector('.profile__edit-button');
 const popup = document.querySelector('.popup');
+const profile = document.querySelector('.profile');
+
+const popupEditButton = document.querySelector('.profile__edit-button');
 const popupCloseButton = document.querySelector('.popup__close-button');
 
-function closePopupOnEscape(e) {
-    if (e.code === 'KeyEscape') {
-        closePopup()
-    }
-    console.log(1);
-}
+const profileName = profile.querySelector('.profile__name');
+const profileProfession = profile.querySelector('.profile__profession');
 
-function openPopup() {
-    popup.classList.remove('popup_hidden');
-    document.addEventListener('keypress', closePopupOnEscape);
-}
+const inputName = popup.querySelector('.popup__input_value_name');
+const inputProfession = popup.querySelector('.popup__input_value_profession');
 
-function closePopup() {
-    popup.classList.add('popup_hidden');
-    document.removeEventListener('keypress', closePopupOnEscape);
-}
-
-button.addEventListener('click', function() {
-    openPopup();
+// Функция открытия popup
+popupEditButton.addEventListener('click', function() {
+  popup.classList.remove('popup_hidden');
+  inputName.value = profileName.textContent;
+  inputProfession.value = profileProfession.textContent;
 });
 
+// Функция закрытия popup
 popupCloseButton.addEventListener('click', function() {
-    closePopup();
+  popup.classList.add('popup_hidden')
 });
 
+// Функция закрытия popup при нажатии на любое место, кроме самого popup
 popup.addEventListener('click', function(e) {
-    if (e.target === e.currentTarget) {
-        popup.classList.add('popup_hidden');
-    }
+  if (e.target === e.currentTarget) {
+      popup.classList.add('popup_hidden');
+  }
 });
